398ff8ffe6979f015459bf179378e1a3b1e9cdd9
LemonPol <lemonpollock@gmail.com>
2024-09-20 00:51:53 -0700

    Implemented equation parsing routine w/ full error handling,

---
+++ app/components/EquationContainer.tsx
@@ -0,0 +1,11 @@
+import EquationBox from "./EquationBox";
+
+function EquationContainer() {
+    return (
+        <div>
+            <EquationBox/>
+        </div>
+    )
+}
+
+export default EquationContainer;
---
+++ app/types/common.ts
@@ -0,0 +1,12 @@
+export interface result {
+    dec: string,
+    hex: string,
+    bin: string
+}
+
+export enum error {
+    UNDEFINED_VARIABLE,
+    MALFORMED_PARENTHESES,
+    MALFORMED_EXPRESSION,
+    INCOMPLETE_EXPRESSION,
+}
